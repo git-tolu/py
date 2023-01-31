5d4aa52334442c648b977fd8008527abb6edbf80
git-tolu <adejumobitoluwanimi2@gmail.com>
2023-01-31 17:47:16 +0100

    python basic

---
+++ index.py
@@ -0,0 +1,36 @@
+# print("hello, puthon")
+# age = 11
+# if age == 10:
+#   print("ten")
+# elif age == 11:
+#   print("eleven")
+# else:
+#   print("invalid")
+
+# quick work
+name = "john smith"
+age = 20
+nemPatient = True
+
+# input function
+# name = input("what is your name? ")
+# print("Hi " + name)
+
+# quick work
+# name = input("what is your name? ")
+# fav_color = input("what is your favourite color? ")
+# print(name + "'s favourite color is " + fav_color)
+
+# program to get birth year
+# birth_yr = input("what is your birth year: ")
+# print(type(birth_yr))
+# age = 2023 - int(birth_yr)
+# print(type(age))
+# print("Your Age is " + str(age))
+
+# quick work
+user_weight = input("what is your weight in pounds: ")
+user_weight_kg = int(user_weight) * 0.45
+print("your weight in kg is " + str(user_weight_kg))
+
+
